9f58a5c6596c17718ef2bff34c1db7cd27deae52
InDepthCode <amrut.work@gmail.com>
2025-04-19 10:44:45 +0530

    first commit

---
+++ src/main/java/com/videocall/user/UserService.java
@@ -0,0 +1,11 @@
+package com.videocall.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private static final List<User> USER_LIST = new ArrayList<>();
+}
---
+++ src/test/java/com/videocall/VidecallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videocall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidecallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
